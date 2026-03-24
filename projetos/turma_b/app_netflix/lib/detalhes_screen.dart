4cfb95f10eb4aaff56415f277d609bb063c2c234
ebarrado <erika_barrado@hotmail.com>
2026-03-24 08:45:08 -0300

    Tela Detalhes Filme Turma B

---
+++ projetos/turma_b/app_netflix/lib/detalhes_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class DetalhesScreen extends StatelessWidget {
+  const DetalhesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("Detalhes do Filme",
+        style: TextStyle(
+          color: Colors.white,
+          fontFamily: "Arial",
+          fontWeight: FontWeight.bold,
+          fontSize: 20
+        ),
+        ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network("https://cdn.ome.lt/26zLLUG_7jUbUagC-jqNb0ZZtuM=/1200x630/smart/filters:format(webp)/extras/conteudos/BANNER_SITE_FILMES_2025_77LfRPS.png",
+            width: double.infinity,
+            height: 220,
+            fit:BoxFit.cover
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Filme em Destaque",
+            style: TextStyle(
+              color: Colors.amber,
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14
+            ),),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pop(context);
+              }, 
+              child: Text("Voltar"))
+          ],
+        ),
+    );
+  }
+}
