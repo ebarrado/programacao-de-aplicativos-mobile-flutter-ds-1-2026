2dab57f19a08ecb87b4880c195002f529e3ac9c3
ebarrado <erika_barrado@hotmail.com>
2026-05-19 10:03:08 -0300

    Atualização Projeto Spotify com Login

--- projetos/turma_a/app_spotify/README.md
+++
@@ -1,3 +0,0 @@
-# app_spotify
-
-A new Flutter project.
--- projetos/turma_a/app_spotify/android/app/src/main/kotlin/com/example/app_spotify/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.app_spotify
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
